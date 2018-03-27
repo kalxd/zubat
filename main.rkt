f875aeae604e9ad3e6f6b5433a07d38c51ff9a67
葉岬 <XGLey@protonmail.com>
2018-03-27 21:34:12 +0800

    删除不要的代码

--- main.rkt
+++ main.rkt
@@ -1,41 +1,5 @@
 #lang racket/base
 
-(provide file->sxml
-         html->xexp)
+(provide all-defined-out)
 
-(require racket/file
-         racket/function
-         racket/contract
-         racket/list
-         html-parsing
-         sxml)
-
-(module+ test
-  (require rackunit)
-  )
-
-(define (maybe/c . cs)
-  (apply or/c (cons #t cs)))
-
-(define/contract (my-head xs)
-  (-> list? (maybe/c string?))
-  (if (empty? xs)
-      #f
-      (first xs)))
-
-(define file->sxml
-  (compose html->xexp
-           (λ (path) (file->string path #:mode 'text))))
-
-(define/contract (element/attr el attr)
-  (-> sxml:element? symbol? (maybe/c string?))
-  (define attr-list (sxml:attr-list el))
-  (ormap (λ (p) (let ([k (first p)]
-                      [v (last p)])
-                  (and (equal? attr k) v)))
-         attr-list))
-
-
-(define doc (file->sxml "./file/sample.html"))
-
-(define main-el (car ((sxpath '(// main)) doc)))
+(require "lib/node.rkt")
