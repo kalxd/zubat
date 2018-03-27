2c5dc6808f80b1dde2769f219839be660c9dd755
葉岬 <XGLey@protonmail.com>
2018-03-27 21:31:59 +0800

    重新导出部分sxml函数

--- lib/node.rkt
+++ lib/node.rkt
@@ -6,7 +6,6 @@
          html/attr)
 
 (require racket/file
-         racket/function
          racket/contract
          racket/list
          html-parsing
@@ -16,33 +15,55 @@
 
 ;;; 测试基本数据
 (module+ test
-  (require rackunit)
+  (require rackunit
+           racket/string)
 
   (define el '(main (@ (id "main-id")
                        (class "container"))
                     "main text"
                     (nav (a (@ (href "link1")) "item 1")
                          (a (@ (href "link2")) "item 2"))))
+
+  (define el1 '(div (@ (class "button")) "primary button"))
+  (define el2 '(input (@ (class "input") (type "text"))))
   )
 
 (define file->sxml
   (compose html->xexp
            (λ (path) (file->string path #:mode 'text))))
 
-(module+ test
-  (define el1 '(div (@ (class "button")) "button"))
-  (define el2 '(input (@ (class "input") (type "text"))))
+;; 元素属性
+(define/contract (html/attr attr el)
+  (-> symbol? sxml:element? (maybe/c string?))
+  (sxml:attr el attr))
 
+(module+ test
   (test-case "html/attr"
     (check-equal? "main-id" (html/attr 'id el))
     (check-equal? "button" (html/attr 'class el1))
     (check-equal? "text" (html/attr 'type el2)))
   )
 
-(define/contract (html/attr attr el)
-  (-> symbol? sxml:element? (maybe/c string?))
-  (define attr-list (sxml:attr-list el))
-  (ormap (λ (p) (let ([k (first p)]
-                      [v (last p)])
-                  (and (equal? attr k) v)))
-         attr-list))
+;; 元素文本
+(define/contract html/text
+  (-> sxml:element? string?)
+  sxml:text)
+
+(module+ test
+  (test-case "html/text"
+    (check-equal? "main text" (html/text el))
+    (check-equal? "primary button" (html/text el1))
+    (check-true (not (non-empty-string? (html/text el2)))))
+  )
+
+;; 元素名称
+(define/contract html/tag-name
+  (-> sxml:element? string?)
+  sxml:ncname)
+
+(module+ test
+  (test-case "html/tag-name"
+    (check-equal? "main" (html/tag-name el))
+    (check-equal? "div" (html/tag-name el1))
+    (check-equal? "input" (html/tag-name el2)))
+  )
