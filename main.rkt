b279c40ea5508d60652d317bad494821d9fc1c11
葉岬 <XGLey@protonmail.com>
2018-04-18 21:00:16 +0800

    入口文件

--- main.rkt
+++ main.rkt
@@ -1,7 +1,7 @@
 #lang racket/base
 
-(provide all-defined-out)
+(provide (all-defined-out))
 
-(require "lib/parse.rkt")
-(require "lib/node.rkt")
-(require "lib/nodeset.rkt")
+(require "lib/parse.rkt"
+         "lib/node.rkt"
+         "lib/nodeset.rkt")
