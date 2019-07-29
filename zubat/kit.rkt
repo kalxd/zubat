277bf7034168ff2f7fc5c1e64b87f888451bcbde
葉岬 <XGLey@protonmail.com>
2019-07-29 20:06:58 +0800

    node-attr

---
+++ zubat/kit.rkt
@@ -0,0 +1,5 @@
+#lang racket/base
+
+(require racket/contract)
+
+;; 判断是否为空元素
--- zubat/node.rkt
+++ zubat/node.rkt
@@ -1,8 +1,26 @@
 #lang racket/base
 
+(require racket/contract
+         racket/string
+         sxml)
+
+(provide (all-defined-out))
+
 (module+ test
   (require "test-kit.rkt")
   (displayln "测试开始！")
   (define el '(main (@ (id "main-id")) "main text"))
   (define el1 '(div (@ (class "button")) "primary button"))
   (define el2 '(input (@ (class "input") (type "text")))))
+
+;; 元素属性
+(define/contract (node-attr el attr)
+  (-> sxml:element? symbol? (or/c #f string?))
+  (let ([attr (sxml:attr el attr)])
+    (and attr (string-trim attr))))
+
+(module+ test
+  (test-case "node-attr"
+    (check-equal? "main-id" (node-attr el 'id))
+    (check-equal? "button" (node-attr el1 'class))
+    (check-equal? #f (node-attr el 'class))))
