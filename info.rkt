d749b2b60f0e945fc61649046f2cb294fe098dfa
荀徒之 <kalxd@126.com>
2021-11-14 19:08:27 +0800

    v0.4.0

--- info.rkt
+++ info.rkt
@@ -8,5 +8,5 @@
                      "racket-doc"))
 (define scribblings '(("scribblings/zubat.scrbl" ())))
 (define pkg-desc "HTML 5简易解释器。")
-(define version "0.3.2")
+(define version "0.4.0")
 (define pkg-authors '(XG.Ley))
